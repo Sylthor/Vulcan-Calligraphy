69895c15f12602d1595564b826c4e55cb6fb47ff
Nicklas Brodin <nicklas.brodin@gmail.com>
2026-07-17 11:19:40 +0200

    Implemented svg nuhm. Will need to refine resizing. Implementing a normalized root optimization now

--- Vulcan.py
+++ Vulcan.py
@@ -25,6 +25,7 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 # Fine tuning parameters
 maximal_window_height = 500
 linewidth = 1.5
+file_ending = ".png"
 
 def subplots(rows,columns,height_scale,override=False,sharex=False,wcs=None,custom_width_factor=1):
     """
@@ -101,9 +102,9 @@ def abspolynomial(x,roots,c):
 
 def curve(roots,ax):
     c_list = np.linspace(-0.1,0.1,10000)
-    x = np.linspace(min(bars),max(bars),1000)
-    roots = bars
-    print(roots)
+    x = np.linspace(min(roots),max(roots),1000)
+    # roots = bars
+    # roots_norm = np.array(roots)/np.max(roots)
     equivalent_width = []
     for i in range(len(c_list)):
         integrated_area = quad(abspolynomial,min(roots),max(roots),args=(roots,c_list[i]))[0]
@@ -166,43 +167,205 @@ def separate_string_into_clumps(string):
                 break
         if not matched:
             # If no clump is matched, add the single character
-            print(string[i])
+            # print(string[i])
             nuhms.append(string[i])
             i += 1
     return nuhms
+
+import re
+import numpy as np
+from xml.etree import ElementTree as ET
+from svgpath2mpl import parse_path
+from matplotlib.patches import PathPatch
+from matplotlib.transforms import Affine2D
+
+
+def _local_tag(elem):
+    return elem.tag.split('}')[-1]
+
+
+def _parse_transform(transform_str):
+    """Parse an SVG transform attribute into a 3x3 affine matrix
+    (matplotlib convention: [[a,c,e],[b,d,f],[0,0,1]])."""
+    mat = np.identity(3)
+    if not transform_str:
+        return mat
+    for cmd, args in re.findall(r'(\w+)\s*\(([^)]*)\)', transform_str):
+        nums = [float(x) for x in re.split(r'[,\s]+', args.strip()) if x]
+        if cmd == 'translate':
+            tx = nums[0]
+            ty = nums[1] if len(nums) > 1 else 0
+            m = np.array([[1, 0, tx], [0, 1, ty], [0, 0, 1]])
+        elif cmd == 'scale':
+            sx = nums[0]
+            sy = nums[1] if len(nums) > 1 else sx
+            m = np.array([[sx, 0, 0], [0, sy, 0], [0, 0, 1]])
+        elif cmd == 'matrix':
+            a, b, c, d, e, f = nums
+            m = np.array([[a, c, e], [b, d, f], [0, 0, 1]])
+        else:
+            m = np.identity(3)
+        mat = mat @ m  # compose left-to-right as SVG spec requires
+    return mat
+
+
+def _collect_paths(elem, parent_matrix, out):
+    """Recursively collect (d_string, fill, matrix) for every <path>."""
+    tag = _local_tag(elem)
+    matrix = parent_matrix @ _parse_transform(elem.get('transform'))
+
+    if tag == 'path' and elem.get('d') is not None:
+        fill = elem.get('fill', 'black')
+        out.append((elem.get('d'), fill, matrix))
+
+    for child in elem:
+        _collect_paths(child, matrix, out)
+
+
+def image_svg(imagename, ax, target_width=None, target_height=None):
+    global width, height, max_height
+
+    tree = ET.parse("alphabet_vectorized//" + imagename)
+    root = tree.getroot()
+
+    if 'viewBox' in root.attrib:
+        minx, miny, svg_w, svg_h = map(float, root.attrib['viewBox'].split())
+    else:
+        def _num(s):
+            return float(re.match(r'[\d.]+', s).group())
+        svg_w = _num(root.get('width', '100'))
+        svg_h = _num(root.get('height', '100'))
+        minx, miny = 0, 0
+    svg_w = svg_w
+    svg_h = svg_h
+    # print(svg_w/8,svg_h/8)
+    # print("Width:",svg_w/8)
+    # --- decide final display size ---
+    if target_width is None and target_height is None:
+        disp_w, disp_h = svg_w, svg_h          # native size
+    elif target_width is not None and target_height is None:
+        disp_w = target_width
+        disp_h = svg_h * (target_width / svg_w)   # keep aspect ratio
+    elif target_height is not None and target_width is None:
+        disp_h = target_height
+        disp_w = svg_w * (target_height / svg_h)   # keep aspect ratio
+    else:
+        disp_w, disp_h = target_width, target_height   # stretch to exact size
+
+    sx = disp_w / svg_w
+    sy = disp_h / svg_h
+
+    horizontal_offset = 55 if imagename == "start.svg" else 0
+
+    if disp_w > width:
+        width = disp_w
+
+    x0 = -disp_w / 2 + horizontal_offset + line
+    y0 = height
+
+    # placement matrix: viewBox coords -> scaled -> translated into data coords
+    placement = np.array([
+        [sx, 0,  x0 - minx * sx],
+        [0,  sy, y0 - miny * sy],
+        [0,  0,  1],
+    ])
+
+    paths_data = []
+    _collect_paths(root, np.identity(3), paths_data)
+
+    for d_str, fill, group_matrix in paths_data:
+        mpl_path = parse_path(d_str)
+        total_matrix = placement @ group_matrix
+        transform = Affine2D(total_matrix) + ax.transData
+        ax.add_patch(PathPatch(
+            mpl_path,
+            transform=transform,
+            facecolor=fill if fill not in (None, 'none') else 'none',
+            edgecolor='none',
+            linewidth=0,
+        ))
+
+    height += disp_h
+    if max_height < height:
+        max_height = height
+def get_svg_size(imagename):
+    tree = ET.parse("alphabet_vectorized//" + imagename)
+    root = tree.getroot()
+
+    if 'viewBox' in root.attrib:
+        __, __, svg_w, svg_h = map(float, root.attrib['viewBox'].split())
+    return svg_w,svg_h
+def calculate_window_size(clumps):
+    figsize_x, figsize_y = get_svg_size("start.svg")
+    line_height = -34
+    for i in range(len(clumps)):
+        # If it is the end of a sentence, add vertical lines, line-break and start new sentence. Reset bars
+        if(clumps[i]=="."):
+            print(i,"Rule A",line_height,figsize_x)
+            line_height += 3*get_svg_size("_"+file_ending)[1]
+            if(line_height>figsize_y):
+                figsize_y = line_height
+            if(i!=len(clumps)-1):
+                figsize_x += get_svg_size("start.svg")[0]*1.1
+                line_height = -34
+                line_height += get_svg_size("newline3"+file_ending)[1]
+        
+        # If the space between two words in the middle of a sentence, add some spacing and reset bars.
+        if(clumps[i]=="_" and clumps[i-1]!="."):
+            print(i,"Rule B")
+            line_height += 2*get_svg_size(clumps[i]+file_ending)[1]
+        
+        # If normal nuhms, just add them.
+        if(clumps[i]!="-" and clumps[i]!="." and clumps[i]!="_"):
+            print(i,"Rule C",line_height)
+            line_height += get_svg_size(clumps[i]+file_ending)[1]
+
+        # If the line exceeds the specified height limit, insert a line-break and continue on new line.
+        if(line_height > maximal_window_height and clumps[i]=="_"):
+            print(i,"Rule D")
+            line_height += get_svg_size("newline2"+file_ending)[1]
+            figsize_x += get_svg_size("start.svg")[0]*1.1
+            if(line_height>figsize_y):
+                figsize_y = line_height
+            line_height = -34
+            # image("horizontal_line"+file_ending,main_axs[0])
+            line_height += get_svg_size("newline"+file_ending)[1]
+        # height_history.append(height)
+    return figsize_x,figsize_y
 
 def image(imagename,ax):
     '''
     The function displays the numh corresponding to the romanized text clump.
     '''
-    img = mpimg.imread(current_directory+"/alphabet//"+imagename)
-    numh_height,nuhm_width,__ = np.shape(img)
+    # print(imagename)
+    if(".svg" in imagename):
+        image_svg(imagename,ax)
+    else:
+        img = mpimg.imread(current_directory+"/alphabet//"+imagename)
+        numh_height,nuhm_width,__ = np.shape(img)
 
-    horizontal_offset = 0
-    if(imagename=="start.png"):
-        horizontal_offset = 7
-    global width
-    global height
-    global max_height
-    if(nuhm_width > width):
-        width = nuhm_width
-    ax.imshow(img,extent=[-nuhm_width/2+horizontal_offset+line,nuhm_width/2+horizontal_offset+line,height+numh_height,height])
-    height += numh_height
-    if(max_height<height):
-        max_height = height
-
-main_fig, main_axs = plt.subplots(1, 1, figsize=(16, 16))
-main_axs = [main_axs]
-# main_fig, main_axs = subplots(1,1,4,override=True)
-main_axs[0].set_axis_off()
+        horizontal_offset = 0
+        if(imagename=="start.png"):
+            horizontal_offset = 7
+        global width
+        global height
+        global max_height
+        if(nuhm_width > width):
+            width = nuhm_width
+        ax.imshow(img,extent=[-nuhm_width/2+horizontal_offset+line,nuhm_width/2+horizontal_offset+line,height+numh_height,height])
+        height += numh_height
+        if(max_height<height):
+            max_height = height
 
 
-string = "Stal Stonn le-matya k'stonn ik tal-tor svi'mazhiv po'ta zeshal aushfa mal-nef-hinek t'sa-veh. Ish-wak svi-aru."
+# string = "Stal Stonn le-matya k'stonn ik tal-tor svi'mazhiv po'ta zeshal aushfa mal-nef-hinek t'sa-veh. Ish-wak svi-aru."
 # string = "Nam-tor Olozhika kluterek t'sha'sutenivaya k'ish she-tor etek s'nezhak isan utvau vah sha'kakhartayek."
 # string = "Rok-tor etek ta sanoi nash uzh-rarav ik ki'fereik-tor nameT'Prion. Dungi olau ish-veh kunli pa'ta paribau k'kanok-veh svi'Shi'svatorai."
 # string = "os-pid-vuhlkansu"
-# string = "na'Gen-lis-tal"
-string = "ahtortorki'fer eik-toraan-aish-ano-de"
+# string = "na'Gen-lis-tal a"
+# string = "ahtortorki'fer eik-toraan-aish-ano-de"
+# string = "mal-nef-hinek"
+# string = "mal mal mal mal"
 # string = "Kol-ut-shan"
 # string = "Ragtaya na'Gen-lis-tal Vuhlkansu eh Gen-lislar os-pid-vuhlkansu ba-golik heh iyi-golik"
 # string = "dif-tor heh smusma"
@@ -213,7 +376,7 @@ string = "ahtortorki'fer eik-toraan-aish-ano-de"
 # string = "ven-dol-tar rufai-bosh. kup-bau-tor ven-dol-tar na'sha'nazh-kap zo-uf nazh-kap. fe-toyeht na'Gen-lis-tal"
 # string = " Nam-tor nen t'tanaf-kitaun t'nash-veh fupa s'vi'le-eshan t'toyeht-irak-dvubikuvan heh tsuri-dvuperuv. Nam-tor nash-kilko tsurkanik fupa s'deshker t'du ha."
 # string = "svi'nash-shi."
-# string = "ketilik pitoh-su'us-ek'tal"
+string = "ketilikpitoh-su'us-ek'tal"
 # string = "galu-dahshaya"
 # string = "ro-kasaya"
 # string = "Nash-vel ra. Sular vi. Fai-tor du n'au ha. Katravahsular t'du ha."
@@ -223,18 +386,32 @@ string = "ahtortorki'fer eik-toraan-aish-ano-de"
 # plt.title("\""+string+"\"\n I have and always shall be your friend.")
 # plt.title("\""+string+"\"\n "+translate)
 
-
 if(string[-1] != "."):
     string += "."
+string = string.replace(" ","_")
+print(string)
 clumps = separate_string_into_clumps(string.lower())
 print(clumps)
+
+figsize_x = 16
+figsize_y = 16
+if file_ending == ".svg":
+    figsize_x,figsize_y = calculate_window_size(clumps)
+print("Figsize:",figsize_x,figsize_y)
+# TODO Look at the size parameters here
+main_fig, main_axs = plt.subplots(1, 1, figsize=(16, 16*figsize_y/figsize_x))
+main_axs = [main_axs]
+# main_fig, main_axs = subplots(1,1,4,override=True)
+main_axs[0].set_axis_off()
 
 width = 0
 height = -34 # Required for the patam. Approximately half of the height of the numh.
 max_height = height
 
 line = 0
-image("start.png",main_axs[0])
+# image("start.png",main_axs[0])
+# image("vect.svg",main_axs[0],target_height=86,target_width=77)
+image("start"+file_ending,main_axs[0])
 height_history = [height]
 bars = [height]
 for i in range(len(clumps)):
@@ -243,22 +420,23 @@ for i in range(len(clumps)):
         if(len(bars)>1):
             bars.append(height)
             curve(bars,main_axs[0])
-        image(" .png",main_axs[0])
-        image(" .png",main_axs[0])
-        image(" .png",main_axs[0])
+        image("_"+file_ending,main_axs[0])
+        image("_"+file_ending,main_axs[0])
+        image("_"+file_ending,main_axs[0])
         if(i!=len(clumps)-1):
             line += width*1.1
             height = -34
-            image("newline3.png",main_axs[0])
+            # image("horizontal_line"+file_ending,main_axs[0])
+            image("newline3"+file_ending,main_axs[0])
             bars = [height]
     
     # If the space between two words in the middle of a sentence, add some spacing and reset bars.
-    if(clumps[i]==" " and clumps[i-1]!="."):
+    if(clumps[i]=="_" and clumps[i-1]!="."):
         if(len(bars)>1):
             bars.append(height)
             curve(bars,main_axs[0])
-        image(clumps[i]+".png",main_axs[0])
-        image(clumps[i]+".png",main_axs[0])
+        image(clumps[i]+file_ending,main_axs[0])
+        image(clumps[i]+file_ending,main_axs[0])
         bars = [height]
     
     # If there are bars, do not add a nuhm, just prepare for the swirls.
@@ -266,24 +444,26 @@ for i in range(len(clumps)):
         bars.append(height)
     
     # If normal nuhms, just add them.
-    if(clumps[i]!="-" and clumps[i]!="." and clumps[i]!=" "):
-        image(clumps[i]+".png",main_axs[0])
+    if(clumps[i]!="-" and clumps[i]!="." and clumps[i]!="_"):
+        image(clumps[i]+file_ending,main_axs[0])
 
     # If the line exceeds the specified height limit, insert a line-break and continue on new line.
-    if(height > maximal_window_height and clumps[i]==" "):
-        image("newline2.png",main_axs[0])
+    if(height > maximal_window_height and clumps[i]=="_"):
+        image("newline2"+file_ending,main_axs[0])
         line += width*1.1
         height = -34
-        image("newline.png",main_axs[0])
+        # image("horizontal_line"+file_ending,main_axs[0])
+        image("newline"+file_ending,main_axs[0])
         bars=[height]
     height_history.append(height)
 
 # The horizontal spine
-main_axs[0].plot([0,width/2+line],[0,0],color="black",linewidth=2.0)
+main_axs[0].plot([0,width/2+line],[240,240],color="black",linewidth=2.0)
 main_axs[0].set_xlim(-width,width+line)
 main_axs[0].set_ylim(max_height,-34)
 main_fig.tight_layout()
 main_fig.savefig(current_directory+"/Generated text.png")
+
 
 import subprocess
 from PIL import Image
